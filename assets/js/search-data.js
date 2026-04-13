786f995138a95d6abaea530519de2c0131c89b8d
tiagosoutorocha-neuro <tiagosoutorocha@gmail.com>
2026-04-13 01:40:21 +0000

    Deploying to gh-pages from @ tiagosoutorocha-neuro/tiagosoutorocha-neuro.github.io@08f71d3ec81ee18e38e0676feb4e928c35ec2c06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,34 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
